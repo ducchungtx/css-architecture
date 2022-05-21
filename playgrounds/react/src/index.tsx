94ac00eb0354507cc791c741e9b9ad1bfbd40b78
Chung Nguyen <zrmedia9@gmail.com>
2022-05-21 11:44:18 +0700

    done react playground

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Button } from '@ds.e/react';
+import '@ds.e/scss/lib/Button.css';
+
+ReactDOM.render(
+  <Button label="Button Example" />,
+  document.getElementById("root")
+)
