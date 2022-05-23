a19b8da96029ad70e6e481acf4c1ffbb95823ab2
Chung Nguyen <zrmedia9@gmail.com>
2022-05-23 11:34:21 +0700

    Add color component

--- packages/react/src/atoms/Color/Color.tsx
+++ packages/react/src/atoms/Color/Color.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import Spacing from '../../foundation/Spacing';
 
 interface ColorProps {
   hexCode: string;
-  width: string;
-  height: string;
+  width?: keyof typeof Spacing;
+  height?: keyof typeof Spacing;
 }
 
-const Color: React.FC<ColorProps> = ({ hexCode, width, height }) => {
+const Color: React.FC<ColorProps> = ({ hexCode, width = Spacing.sm, height = Spacing.sm }) => {
+  const className = `dse-width-${width} dse-height-${height}`;
   return (
-    <div style={{ backgroundColor: hexCode, width, height }}>
+    <div style={{ backgroundColor: hexCode }} className={className}>
     </div>
   );
 }
--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom";
 
 import { Color } from "@ds.e/react";
 
+import '@ds.e/scss/lib/Utilities.css';
+
 ReactDOM.render(
-  <Color hexCode="gray" width="1rem" height="1rem" />,
+  <Color hexCode="gray" width="xxl" height="xxl" />,
   document.getElementById("root")
 )
+
+// .dse-width-xs .dse-width-lg
